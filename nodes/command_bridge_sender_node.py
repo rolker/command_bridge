d204f284288c444bb16e35b15dbf85928ed76be4
Roland Arsenault <roland@ccom.unh.edu>
2019-05-07 23:14:26 -0400

    Fixed race condition when modifiying queue while iterating.

--- nodes/command_bridge_sender_node.py
+++ nodes/command_bridge_sender_node.py
@@ -3,25 +3,30 @@
 import rospy
 from std_msgs.msg import String
 import datetime
-send_queue = {}
+from threading import Lock
 
+send_queue = {}
+lock = Lock()
         
 def sendCommandCallback(data):
     cmd,args = data.data.split(None,1)
     ts = datetime.datetime.utcnow().isoformat()
-    send_queue[cmd] = (ts,args)
+    with lock:
+        send_queue[cmd] = (ts,args)
     
 def update(event):
-    for cmd in send_queue:
-        s = String()
-        s.data = send_queue[cmd][0]+' '+cmd+' '+send_queue[cmd][1]
-        print s
-        command_pub.publish(s)
+    with lock:
+        for cmd in send_queue:
+            s = String()
+            s.data = send_queue[cmd][0]+' '+cmd+' '+send_queue[cmd][1]
+            print s
+            command_pub.publish(s)
 
 def responseCallback(data):
     ts,cmd = data.data.split(None,1)
-    if cmd in send_queue and send_queue[cmd][0] == ts:
-        send_queue.pop(cmd,None)
+    with lock:
+        if cmd in send_queue and send_queue[cmd][0] == ts:
+            send_queue.pop(cmd,None)
 
 rospy.init_node('command_bridge_sender', anonymous=False)
 command_pub = rospy.Publisher('/udp/command',String,queue_size=10)
